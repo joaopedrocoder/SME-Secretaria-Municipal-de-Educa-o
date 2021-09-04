99e5c6c6315250ed8857f5829b613c906e4bfcd1
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-09-04 12:42:02 -0300

    estrutarçao das pastas e criaçao dos arquivos

---
+++ sme/src/constants/urls.jsx
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://hom-escolaaberta.sme.prefeitura.sp.gov.br/api/'
---
+++ sme/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/footer/Footer"
+import Header from "../../components/header/Header"
+import Table from "../../components/table/Table"
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header/>
+            <Table/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default HomePage
