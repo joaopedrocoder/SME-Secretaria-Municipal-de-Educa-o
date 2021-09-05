9abd6292d23018a4b75535b870cb2540e8a48c01
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-09-04 21:26:05 -0300

    criaçao do arquivo services que é responsavel por armazenar todos os gets da aplicaçao

---
+++ sme/src/services/get.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { BASE_URL } from "../constants/urls";
+
+export const getDiretorias = (setData) =>{
+    axios.get(`${BASE_URL}/diretorias/`)
+    .then((res)=>{
+        setData(res)
+    })
+    .catch((error)=>{
+        console.error(error)
+    })
+}
+
+export const getTipoEscola = (setData) =>{
+    axios.get(`${BASE_URL}/smeescolas/`)
+    .then((res)=>{
+        setData(res)
+    })
+    .catch((error)=>{
+        console.error(error)
+    })
+}
+
+export const getTipoEscolaPelaDiretoria = (setData, diretoria) =>{
+    axios.get(`${BASE_URL}/smeescolas/${diretoria}`)
+    .then((res)=>{
+        setData(res)
+    })
+    .catch((error)=>{
+        console.error(error)
+    })
+}
